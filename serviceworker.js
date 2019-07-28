98ad9fa04fa2698ad7782e5e8c2659b39919e417
unknown <hellomittal.mohit@gmail.com>
2019-07-28 12:36:41 +0530

    cache strategy modified

--- serviceworker.js
+++ serviceworker.js
@@ -18,8 +18,14 @@ self.addEventListener('install', function(e) {
 /* Serve cached content when offline */
 self.addEventListener('fetch', function(e) {
   e.respondWith(
-    caches.match(e.request).then(function(response) {
+	fetch(e.request).catch(()=>{
+		return caches.match(e.request)
+	}
+	
+	)
+  
+/*    caches.match(e.request).then(function(response) {
       return response || fetch(e.request);
-    })
+    }) */
   );
 });
